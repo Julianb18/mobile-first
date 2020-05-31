aa973ecbfd032a9b3c7573b90648080028d648c0
Julianb18 <julian141718@gmail.com>
2020-05-31 22:44:57 +0200

    digital clock mobile

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -169,3 +169,133 @@ $("form").submit((e) => {
     $(".message-field .text").css("color", "rgb(34, 192, 255)");
   }
 });
+
+// Digital clock
+
+let Flipper = (function () {
+  function Flipper(node, currentTime, nextTime) {
+    this.isFlipping = false;
+    this.duration = 600;
+    this.flipNode = node;
+    this.frontNode = node.querySelector(".front");
+    this.backNode = node.querySelector(".back");
+    this.setFrontTime(currentTime);
+    this.setBackTime(nextTime);
+  }
+
+  Flipper.prototype.setFrontTime = function (time) {
+    this.frontNode.dataset.number = time;
+  };
+
+  Flipper.prototype.setBackTime = function (time) {
+    this.backNode.dataset.number = time;
+  };
+
+  Flipper.prototype.flipDown = function (currentTime, nextTime) {
+    let _this = this;
+
+    if (this.isFlipping) {
+      return false;
+    }
+
+    this.isFlipping = true;
+    this.setFrontTime(currentTime);
+    this.setBackTime(nextTime);
+    this.flipNode.classList.add("running");
+
+    setTimeout(function () {
+      _this.flipNode.classList.remove("running");
+      _this.isFlipping = false;
+      _this.setFrontTime(nextTime);
+    }, this.duration);
+  };
+  return Flipper;
+})();
+
+let getTimeFromDate = function (date) {
+  return date.toTimeString().slice(0, 8).split(":").join("");
+};
+
+let flips = document.querySelectorAll(".flip");
+let now = new Date();
+let nowTimeStr = getTimeFromDate(new Date(now.getTime() - 1000));
+let nextTimeStr = getTimeFromDate(now);
+let Flippers = Array.from(flips).map(function (flip, i) {
+  return new Flipper(flip, nowTimeStr[i], nextTimeStr[i]);
+});
+
+setInterval(function () {
+  let now = new Date();
+  let nowTimeStr = getTimeFromDate(new Date(now.getTime() - 1000));
+  let nextTimeStr = getTimeFromDate(now);
+
+  for (let i = 0; i < Flippers.length; i++) {
+    if (nowTimeStr[i] === nextTimeStr[i]) {
+      continue;
+    }
+    Flippers[i].flipDown(nowTimeStr[i], nextTimeStr[i]);
+  }
+}, 1000);
+
+// var Flipper = (function () {
+//   function Flipper(node, currentTime, nextTime) {
+//     this.isFlipping = false;
+//     this.duration = 600;
+//     this.flipNode = node;
+//     this.frontNode = node.querySelector(".front");
+//     this.backNode = node.querySelector(".back");
+//     this.setFrontTime(currentTime);
+//     this.setBackTime(nextTime);
+//   }
+//   Flipper.prototype.setFrontTime = function (time) {
+//     this.frontNode.dataset.number = time;
+//   };
+//   Flipper.prototype.setBackTime = function (time) {
+//     this.backNode.dataset.number = time;
+//   };
+//   Flipper.prototype.flipDown = function (currentTime, nextTime) {
+//     var _this = this;
+
+//     if (this.isFlipping) {
+//       return false;
+//     }
+
+//     this.isFlipping = true;
+//     this.setFrontTime(currentTime);
+//     this.setBackTime(nextTime);
+//     this.flipNode.classList.add("running");
+//     setTimeout(function () {
+//       _this.flipNode.classList.remove("running");
+//       _this.isFlipping = false;
+//       _this.setFrontTime(nextTime);
+//     }, this.duration);
+//   };
+//   return Flipper;
+// })();
+
+// var getTimeFromDate = function (date) {
+//   return date.toTimeString().slice(0, 8).split(":").join("");
+// };
+
+// var flips = document.querySelectorAll(".flip");
+// var now = new Date();
+// var nowTimeStr = getTimeFromDate(new Date(now.getTime() - 1000));
+// var nextTimeStr = getTimeFromDate(now);
+// var Flippers = Array.from(flips).map(function (flip, i) {
+//   return new Flipper(flip, nowTimeStr[i], nextTimeStr[i]);
+// });
+
+// setInterval(function () {
+//   var now = new Date();
+//   var nowTimeStr = getTimeFromDate(new Date(now.getTime() - 1000));
+//   var nextTimeStr = getTimeFromDate(now);
+
+//   for (var i = 0; i < Flippers.length; i++) {
+//     if (nowTimeStr[i] === nextTimeStr[i]) {
+//       continue;
+//     }
+//     Flippers[i].flipDown(nowTimeStr[i], nextTimeStr[i]);
+//   }
+// }, 1000);
+
+// export { Flipper };
